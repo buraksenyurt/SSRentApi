975addaf58b6b22bdf7d637681c38b182a640584
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2023-01-17 12:54:07 +0000

    Added some controllers.

---
+++ SSRentApi/Controllers/PortsController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SSRentApi.Data;
+
+namespace SSRentApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PortsController : ControllerBase
+    {
+        protected readonly ApiDbContext _dbContext;
+        public PortsController(IConfiguration configuration)
+        {
+            _dbContext = new ApiDbContext(configuration);
+        }
+
+        [HttpGet]
+        //[Authorize]
+        public IActionResult Get()
+        {
+            return Ok(_dbContext.Ports);
+        }
+    }
+}
